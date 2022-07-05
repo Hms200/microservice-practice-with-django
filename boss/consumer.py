8c9e5e5062038c47d112cf2231fbcc718abe1d87
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-07-05 18:01:37 +0900

    rabbitmq 테스트코드를 실제 데이터 코드로 변경

--- boss/consumer.py
+++ boss/consumer.py
@@ -1,4 +1,6 @@
 import pika
+import json
+from main import Shop, Order, db
 
 params = pika.URLParameters('amqps://evcwolsy:qY9FGzpS2C1qdQVELBmlwNO9g8y6XhXe@dingo.rmq.cloudamqp.com/evcwolsy')
 
@@ -10,7 +12,40 @@ channel.queue_declare(queue='boss')
 
 def callback(ch, method, properties, body):
     print('Received in boss')
+    data = json.loads(body)
     print(body)
+    
+    if properties.content_type == 'shop_created':
+        shop = Shop(id=data['id'], shop_name=data['shop_name'], shop_address=data['shop_address'])
+        db.session.add(shop)
+        db.session.commit()
+        
+    elif properties.content_type == 'shop_updated':
+        shop = Shop.query.get(data['id'])
+        shop.shop_name = data['shop_name']
+        shop.shop_address = data['shop_address']
+        db.session.commit()
+        
+    elif properties.content_type == 'shop_deleted':
+        shop = Shop.query.get(data)
+        db.session.delete(shop)
+        db.session.commit()
+        
+    elif properties.content_type == 'order_created':
+        order = Order(id=data['id'], shop=data['shop'], address=data['address'])
+        db.session.add(order)
+        db.session.commit()
+        
+    elif properties.content_type == 'order_updated':
+        order = Order.query.get(data['id'])
+        order.shop = data['shop']
+        order.address = data['address']
+        db.session.commit()
+    
+    elif properties.content_type == 'order_deleted':
+        order = Order.query.get(data)
+        db.session.delete(order)
+        db.session.commit()
     
 
 channel.basic_consume(queue='boss', on_message_callback=callback, auto_ack=True)
